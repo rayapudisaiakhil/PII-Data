1aef6388a0c38dbe0114e2451901ca6610613b04
Lahari <73251652+LahariBoni@users.noreply.github.com>
2024-04-06 09:57:05 -0400

    Update duplicates_test.py

--- dags/test/duplicates_test.py
+++ dags/test/duplicates_test.py
@@ -5,21 +5,15 @@ from unittest.mock import MagicMock, patch, mock_open
 from dags.src.duplicates import dupeRemoval
 
 class TestDupeRemoval(unittest.TestCase):
-    @patch('dags.src.duplicates.pickle.load')
-    @patch('builtins.open')
-    def test_dupeRemoval(self, mock_open, mock_pickle_load):
-        # Mock DataFrame behavior
-        mock_df_instance = MagicMock()
-        mock_pickle_load.return_value = mock_df_instance
-        
+    @patch('dags.src.duplicates.pickle')
+    @patch('dags.src.duplicates.open')
+    def test_dupeRemoval_function_call(self, mock_open, mock_pickle):
         # Call the function under test
-        input_path = 'dummy_input.pkl'  # Provide a dummy input path
-        output_path = dupeRemoval(input_path)
+        dupeRemoval()
         
-        # Assert your expectations here
-        self.assertIsNotNone(output_path)
-        self.assertTrue(os.path.exists(output_path))
-        # Add more assertions as needed
+        # Assertions
+        mock_open.assert_called_once()
+        mock_pickle.load.assert_called_once()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
