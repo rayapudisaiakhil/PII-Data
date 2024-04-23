cc90d4393df339748bb9fbeb7d1f82451e3d1cc2
Lahari <73251652+LahariBoni@users.noreply.github.com>
2024-04-23 15:03:23 -0400

    Update data_slicing_test.py

--- dags/test/data_slicing_test.py
+++ dags/test/data_slicing_test.py
@@ -20,12 +20,23 @@ class TestDataDownload(unittest.TestCase):
         # Set the mock Blob class to return the mock_blob_instance when instantiated
         mock_blob_class.return_value = mock_blob_instance
 
-        # Mock the os.path.exists to always return True
+        # Prepare the mock input parameters
+        mock_data_dir = '/path/to/data'
+        mock_num_data_points = 10
+        mock_bucket_name = 'your-bucket-name'
+        mock_key_path = '/path/to/key.json'
+
+        # Mock the os.path.exists to always return True for the existence checks
         with mock.patch('os.path.exists', return_value=True):
             # Mock the os.makedirs to do nothing
             with mock.patch('os.makedirs') as mock_makedirs:
                 # Perform the test
-                local_file_path = load_data_from_gcp_and_save_as_json()
+                local_sliced_path, local_cumulative_path, end_index = load_data_from_gcp_and_save_as_json(
+                    data_dir=mock_data_dir,
+                    num_data_points=mock_num_data_points,
+                    bucket_name=mock_bucket_name,
+                    KEY_PATH=mock_key_path
+                )
                 
                 # Verify os.makedirs was not called since the directory exists
                 mock_makedirs.assert_not_called()
@@ -33,8 +44,9 @@ class TestDataDownload(unittest.TestCase):
                 # Verify the blob download was called on the mock_blob_instance
                 mock_blob_instance.download_to_filename.assert_called_once()
 
-                # Check if the function returned the expected path
-                self.assertIn("dags/processed/train.json", local_file_path)
+                # Check if the function returned the expected paths
+                self.assertIn("dags/processed/Fetched/sliced_train_", local_sliced_path)
+                self.assertIn("dags/processed/Fetched/cumulative_train_", local_cumulative_path)
 
 if __name__ == '__main__':
     unittest.main()
