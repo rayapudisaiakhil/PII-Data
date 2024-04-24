a86d95abe46111118a67e2431a3bf750cf7ea8bd
Lahari <73251652+LahariBoni@users.noreply.github.com>
2024-04-24 09:54:00 -0400

    Update data_slicing_test.py

--- dags/test/data_slicing_test.py
+++ dags/test/data_slicing_test.py
@@ -1,18 +1,16 @@
-import os
 import unittest
 from unittest.mock import patch, MagicMock
 from dags.src.data_slicing import load_data_from_gcp_and_save_as_json
 
 class TestLoadDataFromGCPAndSaveAsJSON(unittest.TestCase):
     @patch('dags.src.data_slicing.os.makedirs')
-    @patch('dags.src.data_slicing.os.path.exists', return_value=True)
+    @patch('dags.src.data_slicing.os.path.exists', return_value=False)
     @patch('dags.src.data_slicing.storage.Client')
     @patch('dags.src.data_slicing.storage.Blob')
-    @patch('dags.src.data_slicing.json.load')
+    @patch('dags.src.data_slicing.json.load', side_effect=[[]])
     def test_load_data_from_gcp_and_save_as_json(self, mock_json_load, mock_blob, mock_client, mock_path_exists, mock_makedirs):
         mock_client_instance = mock_client.return_value
         mock_blob_instance = mock_blob.return_value
-        mock_json_load.return_value = []  # Return an empty list for json.load
 
         kwargs = {
             'data_dir': None,
@@ -30,13 +28,8 @@ class TestLoadDataFromGCPAndSaveAsJSON(unittest.TestCase):
         mock_makedirs.assert_called_once()
         mock_path_exists.assert_called()
 
-        # Get the expected file path from the function
-        project_dir = os.getcwd()
-        destination_dir = os.path.join(project_dir, "dags", "processed", "Fetched")
-        expected_file_path = os.path.join(destination_dir, "train.json")
-
         # Ensure that json.load is called with the correct filename
-        mock_json_load.assert_called_once_with(expected_file_path)
+        mock_json_load.assert_called_once_with('/home/runner/work/PII-Data/PII-Data/dags/processed/Fetched/train.json')
 
 if __name__ == '__main__':
     unittest.main()
