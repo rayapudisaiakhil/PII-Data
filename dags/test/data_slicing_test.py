f9b50efdb8d41e99167ade10dfcd32c1b151af1b
Lahari <73251652+LahariBoni@users.noreply.github.com>
2024-04-24 04:53:43 -0400

    Update data_slicing_test.py

--- dags/test/data_slicing_test.py
+++ dags/test/data_slicing_test.py
@@ -8,7 +8,8 @@ class TestLoadDataFromGCPAndSaveAsJSON(unittest.TestCase):
     @patch('dags.src.data_slicing.os.path.exists', return_value=False)
     @patch('dags.src.data_slicing.storage.Client')
     @patch('dags.src.data_slicing.storage.Blob')
-    def test_load_data_from_gcp_and_save_as_json(self, mock_blob, mock_client, mock_path_exists, mock_makedirs, mock_environ_get):
+    @patch('dags.src.data_slicing.json.load', side_effect=[[]])
+    def test_load_data_from_gcp_and_save_as_json(self, mock_json_load, mock_blob, mock_client, mock_path_exists, mock_makedirs, mock_environ_get):
         mock_client_instance = mock_client.return_value
         mock_blob_instance = mock_blob.return_value
 
@@ -28,6 +29,9 @@ class TestLoadDataFromGCPAndSaveAsJSON(unittest.TestCase):
         mock_makedirs.assert_called_once()
         mock_path_exists.assert_called()
         mock_environ_get.assert_called_once_with('GOOGLE_APPLICATION_CREDENTIALS', 'test_key.json')
+
+        # Ensure that json.load is called with the correct filename
+        mock_json_load.assert_called_once_with('/home/runner/work/PII-Data/PII-Data/dags/processed/Fetched/train.json')
 
 if __name__ == '__main__':
     unittest.main()
