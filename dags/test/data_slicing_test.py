74ba597dc1d93861c8c0ef3df7882b566cf7cd83
Lahari <73251652+LahariBoni@users.noreply.github.com>
2024-04-24 10:21:39 -0400

    Update data_slicing_test.py

--- dags/test/data_slicing_test.py
+++ dags/test/data_slicing_test.py
@@ -20,14 +20,8 @@ class TestDataDownload(unittest.TestCase):
         # Set the mock Blob class to return the mock_blob_instance when instantiated
         mock_blob_class.return_value = mock_blob_instance
 
-        # Mock the os.path.exists to return True for train.json and False for other files
-        mock_exists_paths = {
-            os.path.join('dags', 'processed', 'Fetched', 'train.json'): True,
-            os.path.join('dags', 'processed', 'Fetched', 'end_index.txt'): False,
-            os.path.join('dags', 'processed', 'Fetched', 'sliced_train_0_10.json'): False,
-            os.path.join('dags', 'processed', 'Fetched', 'cumulative_train_0_10.json'): False,
-        }
-        with mock.patch('os.path.exists', side_effect=lambda path: mock_exists_paths.get(path, False)):
+        # Mock the os.path.exists to return True for train.json
+        with mock.patch('os.path.exists', return_value=True):
             with mock.patch('os.makedirs'):
                 # Perform the test
                 kwargs = {
@@ -36,15 +30,10 @@ class TestDataDownload(unittest.TestCase):
                     'bucket_name': 'pii_train_data',
                     'KEY_PATH': 'config/key.json'
                 }
-                sliced_path, cumulative_path, end_index = load_data_from_gcp_and_save_as_json(**kwargs)
+                load_data_from_gcp_and_save_as_json(**kwargs)
 
                 # Verify the blob download was called on the mock_blob_instance
                 mock_blob_instance.download_to_filename.assert_called_once()
-
-                # Check if the function returned the expected paths
-                self.assertEqual(sliced_path, os.path.join('dags', 'processed', 'Fetched', 'sliced_train_0_10.json'))
-                self.assertEqual(cumulative_path, os.path.join('dags', 'processed', 'Fetched', 'cumulative_train_0_10.json'))
-                self.assertEqual(end_index, 10)
 
 if __name__ == '__main__':
     unittest.main()
